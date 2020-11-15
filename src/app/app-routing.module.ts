bc8c566dcbccc33ecfa14f860a37e9276f41383a
Rodrigo Sturm <rodrigoks@gmail.com>
2020-11-15 12:00:38 -0300

    criando rotas para navegacao

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(ROUTES),
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
